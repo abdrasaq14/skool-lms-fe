137952a322bde0972f41ea079f56b26a7463b073
Bacteriophagedev <kolawolejaphet4real@gmail.com>
2024-02-22 08:58:05 +0100

    signup page final

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./index.css";
 
 // import TestPage from "./pages/TestPage";
 import LandingPage from "./pages/LandingPage/LandingPage";
+import SignUpPage from "./pages/Signup/SignUpPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         {/* <Route path="/signup" element={<TestPage />} /> */}
+        <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </>
   );
