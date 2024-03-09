edca139bea7b4b63f87cd2ba336fb25b3ddbc5c4
nathanilo <nathanielodion@gmail.com>
2024-03-09 13:52:11 +0100

    Update admin dashboard page

--- src/pages/ApplicationStatesPage/ApplicationStatesPage.tsx
+++ src/pages/ApplicationStatesPage/ApplicationStatesPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import decagonLogo from "/images/decagon-logo.png";
+import ApplicationHeader from "../../components/applicationComponents/ApplicationHeader";
 import axiosInstance from "../../utils/axiosInstance";
 import { Link } from "react-router-dom";
 import PDFDownloadButton from "../../components/DownloadFunction/SingleDownload";
@@ -33,29 +33,21 @@ interface ApplicationDetails {
     phoneNumber: string;
     email: string;
   };
-  //  ProfessionalApplication {
-  //   id: 'b30bc4c9-12c9-4033-8ee3-364473c04d61',
-  //   status: 'pending',
-  //   personalStatement: 'Hello my name is Nkachukwu Nwobi.',
-  //   addQualification: {
-  //     gradeOrCGPA: '4.2',
-  //     fieldOfStudy: 'Chemical Engineering',
-  //     institutionName: 'University of Lagos',
-  //     yearOfGraduation: '2019',
-  //     qualificationType: 'Bachelor Degree',
-  //     countryOfInstitution: 'Nigeria'
-  //   },
+  course: {
+    courseType: string;
+    studyMode: string;
+    courseSearch: string;
+  }[];
 }
 
 function ApplicationStatesPage() {
-  const firstName = localStorage.getItem("name");
   const [applicationData, setApplicationData] = useState<ApplicationDetails[]>(
     []
   );
   const [selectedTab, setSelectedTab] = useState<string | null>("Accepted");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // setData(dummyData);
     const fetchApplications = async () => {
       try {
         const response = await axiosInstance.get(
@@ -64,13 +56,14 @@ function ApplicationStatesPage() {
             withCredentials: true,
           }
         );
-        console.log(response.data);
         setApplicationData(response.data);
-        console.log(applicationData);
       } catch (error) {
         console.error(error);
+      } finally {
+        setLoading(!loading);
       }
     };
+
     fetchApplications();
   }, []);
 
@@ -87,22 +80,10 @@ function ApplicationStatesPage() {
 
   return (
     <>
-      <div className="w-9/12 mx-auto py-6">
-        <div className="flex justify-between">
-          <img src={decagonLogo} alt="Decagon logo" />
+      <div className="w-11/12 mx-auto py-6">
+        <ApplicationHeader header_text="" linkTo="" />
 
-          <div className="flex justify-between gap-6 align-middle items-center">
-            <div className="flex justify-center items-center gap-2">
-              <div className="bg-blue-400 text-blue-600 rounded-full w-10 h-10 flex justify-center items-center">
-                <p className="font-semibold text-base">NN</p>
-              </div>
-
-              <div className="font-normal text-base">{firstName}</div>
-            </div>
-          </div>
-        </div>
-
-        <div className="w-10/12 mx-auto flex items-center justify-center h-12 mt-14 bg-gray-1 rounded-t-2xl">
+        <div className="w-10/12 mx-auto flex items-center justify-center h-12  bg-gray-1 rounded-t-2xl">
           <div
             className={`mr-16 cursor-pointer font-medium hover:bg-white rounded-t-xl w-259px h-fit py-4 px-4 ${
               selectedTab === "Accepted"
@@ -134,56 +115,67 @@ function ApplicationStatesPage() {
             Pending Applications
           </div>
         </div>
+        {loading ? (
+          <div className="flex items-center justify-center h-64">
+            <div className="w-20 h-20 border-t-4 border-b-4 border-green-600 rounded-full text-center animate-spin"></div>
+          </div>
+        ) : filteredData.length === 0 ? (
+          <div className="flex justify-center items-center h-96">
+            <p className="text-2xl font-semibold">No application found</p>
+          </div>
+        ) : (
+          <table className="mx-auto mt-2 w-80%">
+            <thead className="bg-white border-none ">
+              <tr>
+                <th className="border-none  py-6 "></th>
+                <th className="border-none  py-6 ">Name</th>
+                <th className="border-none  py-6 px-16">Program Applied</th>
+                <th className="border-none  py-6 px-16">Degree</th>
+                <th className="border-none  py-6 px-16">Status</th>
 
-        <table className="mx-auto mt-2 w-80%">
-          <thead className="bg-white border-none ">
-            <tr>
-              <th className="border-none  py-6 "></th>
-              <th className="border-none  py-6 ">Name</th>
-              <th className="border-none  py-6 px-16">Program Applied</th>
-              <th className="border-none  py-6 px-16">Degree</th>
-              <th className="border-none  py-6 px-16">Status</th>
-
-              <th className="border-none  py-6 px-16"></th>
-              <th className="border-none  py-6"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredData.map((application: ApplicationDetails) => (
-              <tr key={application.id}>
-                <td className="border-t-0  py-6 ">
-                  <input
-                    className="cursor-pointer"
-                    type="checkbox"
-                    name="application"
-                  />
-                </td>
-                <td className="border-t-0  py-6">{`${application.user.firstName} ${application.user.lastName}`}</td>
-                <td className="border-t-0  py-6 px-16">
-                  {application.addQualification.fieldOfStudy}
-                </td>
-                <td className="border-t-0  py-6 px-16">
-                  {application.addQualification.qualificationType}
-                </td>
-                <td className="border-t-0  py-6 px-16">{application.status}</td>
-
-                <td className="border-t-0  py-6 px-16">
-                  <Link
-                    to={`/admin/dashboard/application-view/${application.id}`}
-                    className="hover:no-underline"
-                  >
-                    <span className="text-green-1 border-2 border-green-border-1 p-2 font-semibold cursor-pointer">
-                      View&nbsp;Application
-                    </span>
-                  </Link>
-                </td>
-                <td className="border-t-0  py-6 cursor-pointer">
-                  <PDFDownloadButton />
-                </td>
+                <th className="border-none  py-6 px-16"></th>
+                <th className="border-none  py-6"></th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {filteredData.map((application: ApplicationDetails) => (
+                <tr key={application.id}>
+                  <td className="border-t-0  py-6 ">
+                    <input
+                      className="cursor-pointer"
+                      type="checkbox"
+                      name="application"
+                    />
+                  </td>
+                  <td className="border-t-0  py-6">{`${application.user.firstName} ${application.user.lastName}`}</td>
+                  <td className="border-t-0  py-6 px-16">
+                    {application.course[0].courseType}
+                  </td>
+                  <td className="border-t-0  py-6 px-16">
+                    {application.course[0].courseSearch}
+                  </td>
+                  <td className="border-t-0  py-6 px-16">
+                    {application.status}
+                  </td>
+
+                  <td className="border-t-0  py-6 px-16">
+                    <Link
+                      to={`/admin/dashboard/application-view/${application.id}`}
+                      className="hover:no-underline"
+                    >
+                      <span className="text-green-1 border-2 border-green-border-1 p-2 font-semibold cursor-pointer">
+                        View&nbsp;Application
+                      </span>
+                    </Link>
+                  </td>
+                  <td className="border-t-0  py-6 cursor-pointer">
+                    <PDFDownloadButton />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
     </>
   );
