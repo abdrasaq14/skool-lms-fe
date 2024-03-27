acdde7feb909e4ec2febcd7a2ea106b8eabc4a21
oderahub <okere_chidera@yahoo.com>
2024-03-27 14:51:24 +0100

    chatcommit

--- src/pages/Messages/ChatInput.tsx
+++ src/pages/Messages/ChatInput.tsx
@@ -11,7 +11,11 @@ const ChatInput: React.FC = () => {
     },
   ]);
 
-  const handleMessageChange = (index: number, newMessage: string, isSender: boolean) => {
+  const handleMessageChange = (
+    index: number,
+    newMessage: string,
+    isSender: boolean
+  ) => {
     const updatedChats = chats.map((chat, i) => {
       if (i === index) {
         return {
@@ -23,18 +27,6 @@ const ChatInput: React.FC = () => {
     });
     setChats(updatedChats);
   };
-
-  // const addChat = () => {
-  //   setChats([
-  //     ...chats,
-  //     {
-  //       senderMessage: "",
-  //       receiverMessage: "",
-  //       senderTime: "",
-  //       receiverTime: "",
-  //     },
-  //   ]);
-  // };
 
   return (
     <div>
@@ -51,9 +43,85 @@ const ChatInput: React.FC = () => {
           }
         />
       ))}
-      {/* <button onClick={addChat}>Add New Chat</button> */}
     </div>
   );
 };
 
 export default ChatInput;
+
+// import React, { useState } from "react";
+// import ChatHeader from "../../components/chat/header";
+
+// interface Chat {
+//   senderMessage: string;
+//   receiverMessage: string;
+//   senderTime: string;
+//   receiverTime: string;
+// }
+
+// const ChatInput: React.FC = () => {
+//   const [chats, setChats] = useState<Chat[]>([
+//     {
+//       senderMessage: "",
+//       receiverMessage: "",
+//       senderTime: "",
+//       receiverTime: "",
+//     },
+//   ]);
+
+//   const handleMessageChange = (
+//     index: number,
+//     newMessage: string,
+//     isSender: boolean
+//   ) => {
+//     const updatedChats = chats.map((chat, i) => {
+//       if (i === index) {
+//         return {
+//           ...chat,
+//           [isSender ? "senderMessage" : "receiverMessage"]: newMessage,
+//           [isSender ? "senderTime" : "receiverTime"]:
+//             new Date().toLocaleTimeString(),
+//         };
+//       }
+//       return chat;
+//     });
+
+//     // If the last chat container is not empty, add a new chat container
+//     const lastChat = updatedChats[updatedChats.length - 1];
+//     const isLastChatEmpty =
+//       lastChat.senderMessage === "" && lastChat.receiverMessage === "";
+//     if (!isLastChatEmpty) {
+//       setChats([
+//         ...updatedChats,
+//         {
+//           senderMessage: "",
+//           receiverMessage: "",
+//           senderTime: "",
+//           receiverTime: "",
+//         },
+//       ]);
+//     } else {
+//       setChats(updatedChats);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       {chats.map((chat, index) => (
+//         <ChatHeader
+//           key={index}
+//           senderMessage={chat.senderMessage}
+//           senderTime={chat.senderTime}
+//           receiverMessage={chat.receiverMessage}
+//           receiverTime={chat.receiverTime}
+//           userName="Jane Doe"
+//           onMessageChange={(newMessage: string) =>
+//             handleMessageChange(index, newMessage, true)
+//           }
+//         />
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default ChatInput;
